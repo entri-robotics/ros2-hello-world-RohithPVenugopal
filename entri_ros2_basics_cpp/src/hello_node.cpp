d01051e29b58925a94e17191ff8d1017151b12eb
RohithPVenugopal <rohithpvenugopal@gmail.com>
2026-04-09 21:06:00 +0530

    Fixed timer syntax and missing semicolon

--- entri_ros2_basics_cpp/src/hello_node.cpp
+++ entri_ros2_basics_cpp/src/hello_node.cpp
@@ -31,7 +31,7 @@ public:
 private:
   // TODO: Define the timer_callback function here
   void timer_callback(){
-    RCLCPP_INFO(this->get_logger(),"Hello, World!");
+    RCLCPP_INFO(this->get_logger(), "Hello, World!");
   }
  
   rclcpp::TimerBase::SharedPtr timer_;
